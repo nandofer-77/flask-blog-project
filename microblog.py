10af8ab3577adb610c3d2095e19cc240cc34f8e0
Nandofer <fernando.tapiat@sanano.usm.cl>
2024-05-28 01:13:26 -0400

    Added a shell context function for the command Flask Shell in microblog.py

--- microblog.py
+++ microblog.py
@@ -1,12 +1,15 @@
 '''
-implementación futura: hacer uso de 
-
-if __name__ == "__main__":
-    main()
+1- uso de decorador @app.shell_context_processor
 
 '''
+import sqlalchemy as sqa
+import sqlalchemy.orm as sqo
+from app import app, db
+from app.models import Usuario, Post
 
-from app import app
+@app.shell_context_processor
+def make_shell_context():
+    return {'sqa':sqa, 'sqo':sqo, 'db':db, 'Usuario':Usuario, 'Post':Post}
 
 def main():
     pass
